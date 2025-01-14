4aeeb4434b798065ef8fe62dc76c5880443973b5
Rizky Dhani <rizkydhani15@gmail.com>
2025-01-14 14:25:17 +0700

    add User CRUD component

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Livewire\Dashboard;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Logout;
-use App\Livewire\Dashboard;
+use App\Livewire\Users\UserIndex;
+use App\Livewire\Users\UserCreate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +13,9 @@ Route::get('/', Login::class)->name('login');
 
 Route::middleware('auth')->group(function () {
     Route::get('dashboard', Dashboard::class)->name('dashboard');
+    Route::get('users', UserIndex::class)->name('users.index');
+    Route::get('users/create', UserCreate::class)->name('users.create');
+    Route::get('users/{userId}/edit', UserCreate::class)->name('users.create');
     Route::view('about', 'about')->name('about');
 
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
