c23d687548b988476c019b23ee6e314bf4a80fa0
Rizky Dhani <rizkydhani15@gmail.com>
2025-01-16 15:43:57 +0700

    change Device to QrCode model

--- app/Models/QrCode.php
+++ app/Models/QrCode.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Device extends Model
+class QrCode extends Model
 {
     protected $guarded = ['id'];
+    protected $table = 'devices';
 }
