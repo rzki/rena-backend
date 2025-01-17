ef956a2676e15e70c57f66ee265a7594625d0a9b
Rizky Dhani <rizkydhani15@gmail.com>
2025-01-17 13:50:27 +0700

    add logo and change the submit button color

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -3,24 +3,18 @@
         <div class="row justify-content-center">
             <div class="col-12 d-flex align-items-center justify-content-center">
                 <div class="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
-                    <div class="text-center text-md-center mb-4 mt-md-0">
-                        <h1 class="mb-3 h3">{{ config('app.name') }}</h1>
+                    <div class="text-center mb-4 mt-md-0">
+                        <img src="{{ asset('images/brand/Rena.webp') }}" class="w-25" alt="">
                     </div>
 
                     <form class="mt-4" wire:submit.prevent="login">
                         @csrf
                         <!-- Form -->
                         <div class="form-group mb-4">
-                            <label for="email">{{ __('Your Email') }}</label>
+                            <label for="email">{{ __('Email') }}</label>
                             <div class="input-group">
-                                <span class="input-group-text" id="basic-addon1">
-                                    <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
-                                        </path>
-                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z">
-                                        </path>
-                                    </svg>
+                                <span class="input-group-text">
+                                    <i class="fas fa-envelope text-white"></i>
                                 </span>
                                 <input name="email" type="email" class="form-control" placeholder="{{ __('Email') }}"
                                     id="email" value="{{ old('email') }}" wire:model="email" required autofocus>
@@ -33,16 +27,10 @@
                         <div class="form-group">
                             <!-- Form -->
                             <div class="form-group mb-4">
-                                <label for="password">{{ __('Your Password') }}</label>
+                                <label for="password">{{ __('Password') }}</label>
                                 <div class="input-group">
-                                    <span class="input-group-text" id="basic-addon2">
-                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                                clip-rule="evenodd">
-                                            </path>
-                                        </svg>
+                                    <span class="input-group-text">
+                                        <i class="fas fa-lock text-white"></i>
                                     </span>
                                     <input name="password" type="password" placeholder="{{ __('Password') }}"
                                         class="form-control" id="password" wire:model="password" required>
@@ -63,7 +51,7 @@
                         </div>
 
                         <div class="d-grid">
-                            <button type="submit" class="btn btn-gray-800">{{ __('Sign in') }}</button>
+                            <button type="submit" class="btn btn-rena">{{ __('Sign in') }}</button>
                         </div>
                     </form>
                 </div>
